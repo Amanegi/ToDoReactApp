19e2cc971d5f6e7c6079276911dc120e32626792
Aman Negi <amannegi@gmail.com>
2020-11-28 20:00:09 +0530

    Updates

---
+++ precache-manifest.0d75bc5dec96ed79ebd1ca29abc1f5a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "814d44953621023f72ca6e10325c1bf2",
+    "url": "/ToDoReactApp/index.html"
+  },
+  {
+    "revision": "6a3a6664dbbfb028651f",
+    "url": "/ToDoReactApp/static/css/main.9d5c62f4.chunk.css"
+  },
+  {
+    "revision": "738e352ef19d3099b459",
+    "url": "/ToDoReactApp/static/js/2.6cd7b635.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToDoReactApp/static/js/2.6cd7b635.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a3a6664dbbfb028651f",
+    "url": "/ToDoReactApp/static/js/main.ed4c7698.chunk.js"
+  },
+  {
+    "revision": "f9f8529c9566c502644b",
+    "url": "/ToDoReactApp/static/js/runtime-main.9acd966c.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/ToDoReactApp/static/media/logo.ee7cd8ed.svg"
+  }
+]);
